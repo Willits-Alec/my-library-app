47348bab1ead9e91319be2634f37e50a8a072b1e
Alec Willits <alecwillits@gmail.com>
2024-07-10 19:39:43 -0600

    last attempt to fix render

---
+++ passport.js
@@ -0,0 +1,12 @@
+const GitHubStrategy = require('passport-github2').Strategy;
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+  },
+  (accessToken, refreshToken, profile, done) => {
+    // Verify callback function
+    return done(null, profile);
+  }
+));
